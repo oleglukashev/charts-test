22a6e18eb3cf67772e705adbc244c1df42c18534
Oleg Lukashev <incognitorus@gmail.com>
2018-02-01 01:35:50 +0400

    add audience service

---
+++ app/services/audience.service.js
@@ -0,0 +1,32 @@
+export default class Auidience {
+  constructor($http, $q, $window) {
+    'ngInject';
+
+    this.$http = $http;
+    this.$window = $window;
+    this.$q = $q;
+  }
+
+  loadData(from, to) {
+    const sessionToken = this.$window.localStorage.getItem('session_token');
+    const deferred = this.$q.defer();
+
+    if (!sessionToken || !from || !to) {
+      deferred.promise;
+    }
+
+    return this.$http({
+      url: `http://localhost:3000/audience`,
+      method: 'POST',
+      data: {
+        session_token: sessionToken,
+        from: from,
+        to: to,
+      },
+    }).then((result) => {
+      return result;
+    }, (error) => {
+      return error;
+    });
+  }
+}
