6c414f2b7fc05ab16b9cb06931cfba262abb9e1e
Oleg Lukashev <incognitorus@gmail.com>
2018-02-01 21:33:59 +0400

    add ng-animate

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import './assets/css/app.scss';
 
 import angularUiRouter from 'angular-ui-router';
 import angularUiBootstrap from 'angular-ui-bootstrap';
+import angularAnimate from 'angular-animate';
 import angularMoment from 'angular-moment';
 import c3Angular from './directives/c3-angular-directives';
 import routing from './app.router';
@@ -11,10 +12,11 @@ import services from './services';
 import constants from './app.constants';
 
 angular
-  .module('app', [angularUiRouter, angularUiBootstrap, angularMoment, dash, c3Angular, services, constants])
+  .module('app', [angularUiRouter, angularUiBootstrap, angularMoment, angularAnimate, dash,
+    c3Angular, services, constants])
   .config(routing)
   .run(($templateCache) => {
     $templateCache.put('dash.view.html', require('./components/dash/dash.view.html'));
     $templateCache.put('layout.view.html', require('./shared/layout.view.html'));
-  })
-  
+  });
+
